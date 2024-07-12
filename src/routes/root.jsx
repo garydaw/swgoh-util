ba70a2d2590757cb7a3d3f192d71afb87406f47c
Gary Daw <garydaw@ntlworld.com>
2024-07-12 20:49:03 +0100

    side menu toggle

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import { Outlet} from "react-router-dom";
 import Navigation from "./navigation";
 
 export default function Root() {
+    const [sidebarOverlay, setsidebarOverlay] = useState(false);
   
+    function sidebarToggle(){
+      setsidebarOverlay(!sidebarOverlay);
+    }
+
     return (
         <div className="container-fluid min-vh-100 d-flex flex-column">
           <div className="row">
             <div className="col-md-6 d-flex align-items-center">
               <h3>Mos Eisley&apos;s Vip Club</h3>
-              <i className="bi-list ms-2"></i>
+              <i role="btn" className={sidebarOverlay ? 'btn bi-list ms-2 d-block' : 'btn bi-list ms-2 d-block d-sm-none'} onClick={sidebarToggle}></i>
             </div>
 
             <div className="col-md-3 offset-md-3 text-end">
@@ -16,7 +22,7 @@ export default function Root() {
             </div>
           </div>
           <div className="row flex-grow-1">
-            <div id="sidebar" className="col-2">
+            <div id="sidebar" className={sidebarOverlay ? 'col-3 sidebar-overlay': "col-2 d-none d-sm-block"}>
               <Navigation></Navigation>
             </div>
             <div id="content" className="col">
