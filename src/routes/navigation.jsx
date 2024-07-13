fd638f6fe62f3327fd1210ca56e3ca5e56fbeecb
Gary Daw <garydaw@ntlworld.com>
2024-07-13 10:00:57 +0100

    Sidebar styling

--- src/routes/navigation.jsx
+++ src/routes/navigation.jsx
@@ -2,13 +2,16 @@ import { Link } from "react-router-dom";
 
 export default function Navigation() {
 
+  const style = "list-group-item list-group-item-action";
+  
   return (
-    <div id="navigation">
-      <ul>
-        <li><Link to={`units`}>Units</Link></li>
-        <li><Link to={`ships`}>Ships</Link></li>
-      </ul>
-      
+
+    <div className="list-group list-group-flush">
+      <a href="#" className={style}><Link to={`units`} className="nav-link">Units</Link></a>
+      <a href="#" className={style}><Link to={`ships`} className="nav-link">Ships</Link></a>
+      <a href="#" className={style}><Link to={`tw`} className="nav-link">Wars</Link></a>
+      <a href="#" className={style}><Link to={`tw/war`} className="nav-link ms-3">War</Link></a>
+      <a href="#" className={style}><Link to={`tw/admin`} className="nav-link ms-3">admin</Link></a>
     </div>
   );
 }
